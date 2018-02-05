58c50fcbadaee590d0f5ac6c70a90b08b4031236
Nichole <n.r.griffith@gmail.com>
2018-02-04 22:20:19 -0600

    button software debounce fully working

--- Counter/Counter/main.asm
+++ Counter/Counter/main.asm
@@ -9,6 +9,15 @@
 .cseg
 .org 0
 
+; 7-segment numbers in format 0b[G][F][E][D][A][B][C][DP]
+; -A-
+;F - B
+; -G-
+;E - C
+; -D-
+
+;numbers: .db 0b01111110, 0b00000110, 0b10111100, 0b10011110, 0b11000110, 0b11011010, 0b11111010, 0b00001110, 0b11111110, 0b11011110
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Pin Assignment Notes
 ; PB0: Shift Register Clock SCK (Pin 15)
@@ -17,228 +26,95 @@
 ; PB5: Input (push-button)
 
 ; Configure PB1, PB2, and PB3 as output pins, and PB5 as input
-      ;sbi   DDRB,0      ; PB1 is now output                        [2 cycles]
-      sbi   DDRB,1      ; PB2 is now output                        [2 cycles]
-	  sbi   DDRB,2      ; PB3 is now output                        [2 cycles]
-	  cbi   DDRB,0      ; PB5 is now input                         [2 cycles]
-
-	  ;sbi   PORTB,0
-	  sbi   PORTB,1
-	  sbi   PORTB,2
+      sbi   DDRB,1      ; PB1 is now output                        [2 cycles]
+	  sbi   DDRB,2      ; PB2 is now output                        [2 cycles]
+	  sbi   DDRB,5      ; PB5 is now output                        [2 cycles]
+	  cbi   DDRB,0      ; PB0 is now input                         [2 cycles]
 
 ; Give registers more meaningful names
 ;.def OutputRegister = r16
 ;clr OutputRegister
 .def Counter = r17
-clr Counter
+.def OnCntr  = r18
+.def OffCntr = r19
+
+FullClear:
+      rcall Clear
+      clr Counter
+	  rjmp Main
+
+Clear:
+      clr OnCntr
+	  clr OffCntr
+	  ret
 
 Main:
-      ; turn on LED 1, turn off LED 2
+	  rcall TurnOff
+	  sbic  PINB,0
+	  rjmp  FullClear
+	  ;
+Zeroth:
+      rcall Button
+	  cpi   Counter,5
+	  brlo  Zeroth
+	  cp    OnCntr,OffCntr
+	  brlo  FullClear
+	  cbi   PORTB,1
+	  ;
+	  rcall Clear
+	  ;.
+First:             ; Check if t <= 1 sec
+	  rcall Button
+	  cpi   Counter,100
+	  brlo  First
+	  cp    OnCntr,OffCntr
+	  brlo  FullClear
+	  cbi   PORTB,2     ; Turn on first light if t <= 1 sec
+	  ;
+	  clr OnCntr
+	  clr OffCntr
+	  ;
+Second:             ; Check if 1 sec < t < 2 sec
+	  rcall Button
+	  cpi   Counter,200
+	  brlo  Second
+	  cp    OnCntr,OffCntr
+	  brlo  FullClear
+	  cbi   PORTB,5     ; Turn on second light if 1 sec < t < 2 sec
+	  ;
+	  sbic  PINB,0      ; Check if button is still pushed
+	  rjmp FullClear
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	  ; delay to see light
 	  clr Counter
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
+Third:
+      inc   Counter
 	  rcall delay_long
-	  ;
+	  cpi   Counter,100
+	  brlo Third
+      ;
+	  rjmp FullClear
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Button:
+      inc Counter
       sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
+	  inc OnCntr
+	  sbic  PINB,0
+	  inc OffCntr
 	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-	  cpi Counter,30
-      brge TurnOn
-	  ;
-Next:
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-      sbis  PINB,0
-	  inc Counter
-	  sbis  PINB,0
-	  rcall delay_long
-	  ;
-	  cpi Counter,5
-      brge TurnOnTwo
-	  ;
-	  rjmp TurnOff
-	  rjmp Main
+	  ret
 
 TurnOff:
       sbi PORTB,1
 	  sbi PORTB,2
-	  rjmp Main
-
-TurnOn:
-      cbi PORTB,1
-	  rjmp Next
-
-TurnOnTwo:
-      cbi PORTB,2
-	  rjmp Main
+	  ;sbi PORTB,5
+	  ret
 
 ; delay for ~10ms
 delay_long:          ;
-      ldi   r23,54      ; r23 <-- Counter for outer loop            [1 cycle ]
-  d1: ldi   r24,2     ; r24 <-- Counter for level 2 loop          [1 cycle ]
-  d2: ldi   r25,22      ; r25 <-- Counter for inner loop            [1 cycle ]
+      ldi   r23,11      ; r23 <-- Counter for outer loop            [1 cycle ]
+  d1: ldi   r24,13     ; r24 <-- Counter for level 2 loop          [1 cycle ]
+  d2: ldi   r25,174      ; r25 <-- Counter for inner loop            [1 cycle ]
   d3: dec   r25         ; decrement r25                             [1 cycle ]
       nop               ; no operation                              [1 cycle ]
 	  brne  d3          ;                                           [1 cycle if 0, 2 cycles other]
